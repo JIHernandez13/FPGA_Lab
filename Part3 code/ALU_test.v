caaa9723889fd835a585d9b6c0f603e8b52a4754
Jesus Hernandez <jesus.hernandez@automotivepower.com>
2021-03-07 17:41:48 -0800

    Added comments

--- Part3 code/ALU_test.v
+++ Part3 code/ALU_test.v
@@ -6,6 +6,27 @@ reg sw0,sw1,sw2,sw3,sw4,sw5,sw6,sw7,sw8,sw9,sw10,sw11,sw12,sw13,sw14,sw15;  // 1
 alu_top alu_uut(sw0,sw1,sw2,sw3,sw4,sw5,sw6,sw7,sw8,sw9,sw10,sw11,sw12,sw13,sw14,sw15,btn0,btn1,ssd,comparator_out);
 initial
 begin
+
+/* 
+
+We should split the testing like this,
+
+ADDITION
+    test that normal 8-bit addition works....
+    test that 8-bit addition works w/ overflow (i.e. 0xff + 0x01 = 0x00 w/ C = 1)
+
+SUBTRACTION
+    test that normal 8-bit subtraction works....
+    test that 8-bit subtraction works w/ underflow (i.e. 0xff + 0x01 = 0x00 w/ C = 1)
+
+COMPARATOR
+    x>y ? this should be pretty easy to test with different inputs
+
+MULTIPLEXER
+Multiplex in the 8 bit result and switch between upper and lower 4 bits to show out.
+
+ */
+
 //todo: get the decimal/ hex numbers out here so i'm not translating this everytime please.
 sw7=0; sw6=0; sw5=1; sw4=0; sw3=0; sw2=1; sw1=1; sw0=1; //x  //note: msb or lsb??
 sw15=0; sw14=0; sw13=1; sw12=0; sw11=0; sw10=1; sw9=1; sw8=1; //y
